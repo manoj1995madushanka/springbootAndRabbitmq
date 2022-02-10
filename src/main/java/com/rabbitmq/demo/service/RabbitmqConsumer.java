51560d82a8c29452354f5000c363e9e3ed9a638b
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-11 00:44:09 +0530

    consume message to headers exchange using rabbitListener

--- src/main/java/com/rabbitmq/demo/service/RabbitmqConsumer.java
+++ src/main/java/com/rabbitmq/demo/service/RabbitmqConsumer.java
@@ -4,6 +4,11 @@ import com.rabbitmq.demo.domain.Person;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 @Service
 public class RabbitmqConsumer {
 
@@ -15,6 +20,21 @@ public class RabbitmqConsumer {
     public void getMessage(Person person) // in here we know about message data type we can use that also
             // means we can use byte[] directly without Person object
     {
+
+        System.out.println(person.getName());
+    }
+
+    /**
+     * consume message from headers exchange
+     * */
+    @RabbitListener(queues = "Mobile")
+    public void getHeaderExchangeMessage(byte[] message) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(message);
+        ObjectInput objectInput = new ObjectInputStream(byteArrayInputStream);
+
+        Person person = (Person) objectInput.readObject();
+        objectInput.close();
+        byteArrayInputStream.close();
 
         System.out.println(person.getName());
     }
