99c752590eb40897b3d7c791f374af44cb8242c3
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-11 00:21:45 +0530

    consume message using RabbitListener

---
+++ src/main/java/com/rabbitmq/demo/service/RabbitmqConsumer.java
@@ -0,0 +1,25 @@
+package com.rabbitmq.demo.service;
+
+import com.rabbitmq.demo.domain.Person;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+@Service
+public class RabbitmqConsumer {
+
+    /**
+     * listen message of Mobile queue
+     * we do not need to call this because @RabbitListener listen to queue eventually
+     * */
+    @RabbitListener(queues = "Mobile")// this will continuously listen to the Mobile queue
+    public void getMessage(Person person) // in here we know about message data type we can use that also
+            // means we can use byte[] directly without Person object
+    {
+
+        System.out.println(person.getName());
+    }
+}
