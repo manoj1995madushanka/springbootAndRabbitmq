bfe16a4142a05cd99fb5756f3915a092b8b449a3
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-11 00:37:13 +0530

    publish message to headers exchange using rabbitTemplate

--- src/main/java/com/rabbitmq/demo/controller/TestController.java
+++ src/main/java/com/rabbitmq/demo/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.rabbitmq.demo.controller;
 
 import com.rabbitmq.demo.domain.Person;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,6 +36,33 @@ public class TestController {
         rabbitTemplate.convertAndSend("Topic-Exchange","tv.mobile.ac",person);// pass to topic exchange
 
         // headers exchange is sightly different from above approaches
+
+        return "SUCCESS";
+    }
+
+    /**
+     * publisher to headers exchange
+     * */
+    @GetMapping("/testHeadersExchange/{name}")
+    public String testHeaderExchange(@PathVariable("name") String name) throws IOException {
+        Person person = new Person(2l,name);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutput objectOutput = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutput.writeObject(person);
+        objectOutput.flush();
+        objectOutput.close();
+
+        byte[] messageArray = byteArrayOutputStream.toByteArray();
+        byteArrayOutputStream.close();
+
+        // publish object byte array to headers exchange
+        Message message = MessageBuilder.withBody(messageArray)
+                .setHeader("item1","mobile")
+                .setHeader("item2","television")
+                .build();
+
+        rabbitTemplate.send("Headers-Exchange","",message);
 
         return "SUCCESS";
     }
