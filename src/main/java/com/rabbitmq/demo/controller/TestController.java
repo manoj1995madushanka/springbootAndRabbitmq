8c7d058a453a3c6e1c2192a57f2cf6ccc2dd8413
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-11 00:06:18 +0530

    publish message using RabbitTemplate

--- src/main/java/com/rabbitmq/demo/controller/TestController.java
+++ src/main/java/com/rabbitmq/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.rabbitmq.demo.controller;
 
+import com.rabbitmq.demo.domain.Person;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class TestController {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @GetMapping("/test/{name}")
-    public String testAPI(@PathVariable("name") String name)
-    {
+    public String testAPI(@PathVariable("name") String name) {
+        Person person = new Person(1l, name);
+
+        // this convert and send using simple message converter
+        // simple message converter only supports string,byte array and serializable object
+        // so Person should be implemented Serializable interface
+        rabbitTemplate.convertAndSend("Mobile", person); // pass message to queue
+
+        rabbitTemplate.convertAndSend("Direct-Exchange","mobile",person);// pass to direct exchange
+        rabbitTemplate.convertAndSend("Fanout-Exchange","",person);// pass to fanout exchange
+        rabbitTemplate.convertAndSend("Topic-Exchange","tv.mobile.ac",person);// pass to topic exchange
+
+        // headers exchange is sightly different from above approaches
+
         return "SUCCESS";
     }
 }
